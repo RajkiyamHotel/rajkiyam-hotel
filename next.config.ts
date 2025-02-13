c6abc3a3f843f69dfcd0a779b72e05849a967673
bitbucket ofc <pankajkumar199528@gmail.com>
2025-02-13 12:01:48 +0530

    image loading fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   domains: ['ik.imagekit.io'], // Add this domain to the list
-  // },
+  images: {
+    domains: ['ik.imagekit.io'], // Add this domain to the list
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@
 // import HotelImage from '../../public/images/rec.jpg'
 import ImageCarousel from "@/components/body/ImageCarousel";
 import HotelDetails from "@/components/body/HotelDetails";
-import {image} from "@/components/utils/image";
+// import {image} from "@/components/utils/image";
+import { image1 } from "@/components/utils/image";
 import ReviewPage from "@/components/reviews/ReviewPage";
 const Home: React.FC = () => {
 
   return (
     <div>
       {/* <Image alt="hotel" src={HotelImage}/> */}
-      <ImageCarousel images={image} />
+      <ImageCarousel images={image1} />
       <HotelDetails />
       <ReviewPage />
     </div>
